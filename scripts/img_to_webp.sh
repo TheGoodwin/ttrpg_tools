a4d31398cba02db6c958ee63c43819c4c1c02a42
Jean-Paul Goodwin <goodwinjeanpaul@outlook.fr>
2024-05-01 16:30:44 +0200

    Create a folder to output webp files

--- scripts/img_to_webp.sh
+++ scripts/img_to_webp.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+webp_conversion_folder='webp_conversion'
+
+
+
 for file in *; do
-    cwebp -q 80 "$file" -o "${file%.*}.webp"
+    mkdir -p $webp_conversion_folder;
+    if [[ "$file" != "$webp_conversion_folder" ]]; then
+        cwebp -q 80 "$file" -o "$webp_conversion_folder/${file%.*}.webp"
+    fi
+    
 done
